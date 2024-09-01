f8b9fe51a7aa6787d5c2f37a87ea0c04731118f4
ShubhamYv <shubhamyadav32100@gmail.com>
2024-09-01 19:59:34 +0530

    Implement rate limiting middleware to restrict requests to 20 per 5 minutes per IP

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 const express = require('express')
 const { ServerConfig, Logger } = require('./config')
 const apiRoutes = require('./routes')
+const rateLimit = require('express-rate-limit');
 
 const app = express()
+
+const limiter = rateLimit({
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 20, // limit each IP to 20 requests per 'window' (here 5 minutes)
+});
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use("/api", apiRoutes)
+app.use(limiter);
+
+app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
   console.log(`Port is running on ${ServerConfig.PORT}`);
