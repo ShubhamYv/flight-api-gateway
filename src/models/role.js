71f855263379c36733d97067e7bce82f931e9fd4
ShubhamYv <shubhamyadav32100@gmail.com>
2024-09-02 14:40:21 +0530

    Added 'Role' model and migration

---
+++ src/models/role.js
@@ -0,0 +1,21 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Role extends Model {
+    static associate(models) {
+      // define association here
+    }
+  }
+  Role.init({
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    }
+  }, {
+    sequelize,
+    modelName: 'Role',
+  });
+  return Role;
+};
